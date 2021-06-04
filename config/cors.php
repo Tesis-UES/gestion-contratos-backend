7a9b40f315fd737f0af718602d84b68c0917e784
Walter Luna <wpenate@applaudostudios.com>
2021-06-03 22:55:04 -0600

    🔧 CORS: update allowed origins (2)

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['localhost:3000', 'https://gestion-contratos.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000', 'https://gestion-contratos.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
