3e9ef0df0f069b3f4c87bd46065c41da0d93f636
AlexCasa <guillermobrs@gmail.com>
2021-06-05 17:52:45 -0600

    ✨ Feature: add Faculty end points and seeder

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EscalafonController;
+use App\Http\Controllers\FacultyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,11 +25,20 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 // Protected Routes 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    //Rutas que maneja el catalogo de escalafones
     Route::post('/escalafones', [EscalafonController::class, 'store']);
     Route::get('/escalafones', [EscalafonController::class, 'all']);
     Route::get('/escalafones/{id}', [EscalafonController::class, 'show']);
     Route::put('/escalafones/{id}', [EscalafonController::class, 'update']);
     Route::delete('/escalafones/{id}', [EscalafonController::class, 'destroy']);
+
+    //Rutas que maneja el catalogo de facultades
+    Route::post('/faculty', [FacultyController::class, 'store']);
+    Route::get('/faculty', [FacultyController::class, 'all']);
+    Route::get('/faculty/{id}', [FacultyController::class, 'show']);
+    Route::put('/faculty/{id}', [FacultyController::class, 'update']);
+    Route::delete('/faculty/{id}', [FacultyController::class, 'destroy']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
