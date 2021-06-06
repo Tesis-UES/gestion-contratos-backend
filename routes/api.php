c83b11f81a078caf2445c2626fa6687e1af78101
AlexCasa <guillermobrs@gmail.com>
2021-06-06 15:36:31 -0600

    🌱 seed: Update

--- routes/api.php
+++ routes/api.php
@@ -35,13 +35,19 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::delete('/escalafones/{id}', [EscalafonController::class, 'destroy']);
 
     //Rutas que maneja el catalogo de facultades
+
+    Route::post('/faculties', [FacultyController::class, 'store']);
+    Route::get('/faculties', [FacultyController::class, 'all']);
+    Route::get('/faculties/{id}', [FacultyController::class, 'show']);
+    Route::put('/faculties/{id}', [FacultyController::class, 'update']);
+    Route::delete('/faculties/{id}', [FacultyController::class, 'destroy']);
+
+    //Rutas que maneja el catalogo de escuelas pertenecientes a facultades
     Route::post('/faculties/{id}/schools', [SchoolController::class, 'store']);
     Route::get('/faculties/{id}/schools', [SchoolController::class, 'all']);
     Route::get('/schools/{id}', [SchoolController::class, 'show']);
     Route::put('/schools/{id}', [SchoolController::class, 'update']);
     Route::delete('/schools/{id}', [SchoolController::class, 'destroy']);
-
-    //Rutas que maneja el catalogo de escuelas pertenecientes a facultades
 
     
 });
