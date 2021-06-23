f494fa30bb2a6c0bd76a0d93ff9e08a3e23e4531
Walter Luna <mywalter97@gmail.com>
2021-06-23 07:44:42 -0600

    ✨ Add endpoints to manage central authorities

---
+++ app/Http/Controllers/CentralAuthorityController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Traits\WorklogTrait;
+use App\Models\CentralAuthority;
+use Illuminate\Http\Request;
+
+class CentralAuthorityController extends Controller
+{
+    use WorklogTrait;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function all()
+    {
+        $authorities = CentralAuthority::all();
+        $this->RegisterAction('El usuario ha consultado el catalogo de autoridades centrales');
+        return response($authorities, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'position'    => 'required|string|max:120',
+            'firstName'   => 'required|string|max:60',
+            'middleName'  => 'string|max:60',
+            'lastName'    => 'required|string|max:120',
+            'dui'         => 'required|string|max:20',
+            'nit'         => 'required|string|max:20'
+        ]);
+        $newAuthority = CentralAuthority::create($request->all());
+
+        $this->RegisterAction('El usuario ha creado la autoridad central con ID: ' . $newAuthority['id']);
+        return response($newAuthority, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CentralAuthority  $centralAuthority
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $authority = CentralAuthority::findOrFail($id);
+        $this->RegisterAction('El usuario ha visto la autoridad central con ID: ' . $id);
+        return response($authority, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CentralAuthority  $centralAuthority
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'position'    => 'required|string|max:120',
+            'firstName'   => 'required|string|max:60',
+            'middleName'  => 'string|max:60',
+            'lastName'    => 'required|string|max:120',
+            'dui'         => 'required|string|max:20',
+            'nit'         => 'required|string|max:20'
+        ]);
+
+        $authority = CentralAuthority::findOrFail($id);
+        $authority->update($request->all());
+        $this->RegisterAction('El usuario ha actualizado la autoridad central con ID : ' . $id);
+        return response($authority, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CentralAuthority  $centralAuthority
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $authority = CentralAuthority::findOrFail($id);
+        $authority->delete();
+
+        $this->RegisterAction('El usuario ha borrado la autoridad central con ID: ' . $id);
+        return response(null, 204);
+    }
+}
