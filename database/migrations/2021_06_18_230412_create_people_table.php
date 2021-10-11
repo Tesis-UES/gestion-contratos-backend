442874a3a2878ba041fc1dba77a0a9b906d47814
AlexCasa <guillermobrs@gmail.com>
2021-10-11 17:49:19 -0600

    fix

--- database/migrations/2021_06_18_230412_create_people_table.php
+++ database/migrations/2021_06_18_230412_create_people_table.php
@@ -51,7 +51,7 @@ class CreatePeopleTable extends Migration
             $table->string('bank_account_number',120)->nullable();;
             $table->string('bank_account',250)->nullable();
             $table->string('status',50)->default('Registrando');
-            $table->boolen('is_employe');
+            $table->boolean('is_employe');
             $table->softDeletes();
             $table->timestamps();
         });
