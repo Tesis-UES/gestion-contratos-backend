c49ed5665d10847d321c54543a6a903d100b0d0d
Walter Luna <mywalter97@gmail.com>
2022-01-14 21:57:14 -0600

    ✨ add endpoint to add detail to TA hiring request

--- app/Http/Controllers/HiringRequestDetailController.php
+++ app/Http/Controllers/HiringRequestDetailController.php
@@ -6,6 +6,7 @@ use App\Constants\ContractType;
 use App\Constants\GroupStatus;
 use App\Constants\PersonValidationStatus;
 use App\Http\Requests\UpdateSPNPRequestDetails;
+use App\Http\Requests\UpdateTARequestDetails;
 use App\Http\Requests\UpdateTIRequestDetails;
 use App\Http\Traits\WorklogTrait;
 use App\Models\Activity;
@@ -135,13 +136,11 @@ class HiringRequestDetailController extends Controller
         }
         if ($weeklyHours > 5) {
             DB::rollBack();
-            return response(['message' => 'Un empleado no puede trabajar mas de 5 horas por semana por esta modalidad'], 400);
+            return response(['message' => 'Un empleado no puede trabajar mas de 5 horas por semana en esta modalidad'], 400);
         }
 
         $savedDetail->groups()->saveMany($groups);
         $savedDetail->groups = $groups;
-
-        $savedDetails[] = $savedDetail;
 
         $user = Auth::user();
         $hiringRequest = HiringRequest::with(['contractType', 'school',])->findOrFail($id);
@@ -157,9 +156,81 @@ class HiringRequestDetailController extends Controller
             return response(['message' => 'No puede agregar detalles a una solicitud de contratacion con estado: "' . $requestStatus->name . '"'], 400);
         }
 
-        $this->RegisterAction("El usuario ha actualizado los detalles de la solicitud de contratación con id: " . $id, "high");
         $this->RegisterAction("El usuario ha agregado a un docente a la solicitud de contratación con id: " . $id, "high");
         DB::commit();
-        return response($savedDetails);
+        return response($savedDetail);
+    }
+
+    public function addTARequestDetails($id, UpdateTARequestDetails $request)
+    {
+        DB::beginTransaction();
+        $validatedDetail = $request->validated();
+
+        $person = Person::with(['employee', 'employee.staySchedules'])->findOrFail($validatedDetail['person_id']);
+        // TODO: Verificar que tipo de empleado puede agregarse en solicitudes de tiempo integral
+        if (false) {
+            DB::rollBack();
+            return response(['message' => 'Solo empleados del tipo <TIPO> pueden contratarse por esta modalidad (Tiempo Adicional)'], 400);
+        } elseif ($person->status != PersonValidationStatus::Validado) {
+            DB::rollBack();
+            return response(['message' => 'No se han validado los datos de la persona con id' . $person->id], 400);
+        }
+
+        $savedDetail = HiringRequestDetail::create($validatedDetail);
+
+        foreach ($validatedDetail['activities'] as $activityName) {
+            $activity = Activity::where('name', 'ilike', $activityName)->first();
+            if (!$activity) {
+                $activity = Activity::create(['name' => $activityName]);
+            }
+            $activities[] = $activity;
+        }
+        $savedDetail->activities()->saveMany($activities);
+        $savedDetail->activities = $activities;
+
+        $weeklyHours = 0;
+        foreach ($validatedDetail['group_ids'] as $groupId) {
+            $group = Group::with('schedule')->findOrFail($groupId);
+            array_reduce($group->schedule->toArray(), function ($pv, $schedule) {
+                return $pv + (strtotime($schedule['finish_hour']) - strtotime($schedule['start_hour'])) / 3600;
+            }, $weeklyHours);
+
+            if ($group->status != GroupStatus::SDA) {
+                DB::rollBack();
+                return response(['message' => 'El grupo con id ' . $groupId . ' ya tiene un docente asignado'], 400);
+            }
+            $scheduleDetails = $person->employee->staySchedules->last()->scheduleDetails;
+
+            // TODO: Validar que los grupos a contratar no choquen con permanencia
+            $group->people_id = $validatedDetail['person_id'];
+            $group->status = GroupStatus::DASC;
+            $group->save();
+            $groups[] = $group;
+        }
+        if ($weeklyHours > 10) {
+            DB::rollBack();
+            return response(['message' => 'Un empleado no puede trabajar mas de 40 horas por mes en esta modalidad'], 400);
+        }
+
+        $savedDetail->groups()->saveMany($groups);
+        $savedDetail->groups = $groups;
+
+        $user = Auth::user();
+        $hiringRequest = HiringRequest::with(['contractType', 'school',])->findOrFail($id);
+        $requestStatus = $hiringRequest->getLastStatusAttribute();
+        if ($hiringRequest->contractType->name != ContractType::TA) {
+            DB::rollBack();
+            return response(['message' => 'Debe seleccionar un contrato del tipo "' . ContractType::TA . '"'], 400);
+        } elseif ($user->school->id != $hiringRequest->school->id) {
+            DB::rollBack();
+            return response(['message' => 'No puede agregar detalles a una solicitud de contratacion de otra escuela'], 400);
+        } elseif ($requestStatus->order > 2) {
+            DB::rollBack();
+            return response(['message' => 'No puede agregar detalles a una solicitud de contratacion con estado: "' . $requestStatus->name . '"'], 400);
+        }
+
+        $this->RegisterAction("El usuario ha agregado a un docente a la solicitud de contratación con id: " . $id, "high");
+        DB::commit();
+        return response($savedDetail);
     }
 }
---
+++ app/Http/Requests/UpdateTARequestDetails.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTARequestDetails extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'start_date'     => 'required|date_format:Y-m-d',
+      'finish_date'    => 'required|date_format:Y-m-d|after:start_date',
+      'position'       => 'required|string',
+      'weekly_hours'   => 'required|numeric|gt:1.0|max:10',
+      'work_weeks'     => 'required|numeric|gt:1',
+      'hourly_rate'    => 'required|numeric|gt:0.0',
+      'person_id'      => 'required|integer|gte:1',
+      'activities'     => 'required|array|min:2',
+      'activities.*'   => 'required|string|distinct',
+      'group_ids'      => 'required|array|min:1',
+      'group_ids.*'    => 'required|integer|distinct|gte:1',
+    ];
+  }
+}
