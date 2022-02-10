972ab3ba18e035c5c43bcf84e042569c23e6c8b2
AlexCasa <guillermobrs@gmail.com>
2022-02-09 18:21:42 -0600

    mejorando seeder de prueba de posiciones

--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -27,38 +27,40 @@ class PositionSeeder extends Seeder
         $profesorLab = Position::create(['name' => 'Profesor de laboratorio']);
         $profesorLab->activities()->createMany([
             ['name' => 'Impartir laboratorio de la asignatura'],
-            ['name' => 'Preparar material de apoyo'],
-            ['name' => 'Elaborar evlualaciones'],
-            ['name' => 'Administrar evaluaciones'],
-            ['name' => 'Calificar evaluaciones'],
-            ['name' => 'Atender consultas'],
+            ['name' => 'Preparar material de apoyo para el laboratorio'],
+            ['name' => 'Elaborar evlualaciones de labaratorio'],
+            ['name' => 'Administrar evaluaciones de laboratorio'],
+            ['name' => 'Calificar evaluaciones de laboratorio'],
+            ['name' => 'Atender consultas de labortario'],
         ]);
         
         $profesorDiscusion = Position::create(['name' => 'Profesor de discusión']);
         $profesorDiscusion->activities()->createMany([
             ['name' => 'Impartir discusiones de la asignatura'],
-            ['name' => 'Preparar material de apoyo'],
-            ['name' => 'Elaborar evlualaciones'],
-            ['name' => 'Administrar evaluaciones'],
-            ['name' => 'Calificar evaluaciones'],
-            ['name' => 'Atender consultas'],
+            ['name' => 'Preparar material de apoyo de discucion '],
+            ['name' => 'Elaborar evaluaciones de discusión'],
+            ['name' => 'Administrar evaluaciones de discusión'],
+            ['name' => 'Calificar evaluaciones de discusión'],
+            ['name' => 'Atender consultas de discusión'],
         ]);        
         
         $profesor = Position::create(['name' => 'Profesor']);
         $profesor->activities()->createMany([
             ['name' => 'Impartir clases teoricas de la asignatura '],
-            ['name' => 'Preparar material de apoyo'],
-            ['name' => 'Elaborar evlualaciones'],
-            ['name' => 'Administrar evaluaciones'],
-            ['name' => 'Calificar evaluaciones'],
-            ['name' => 'Atender consultas'],
+            ['name' => 'Preparar material de apoyo de asignatura'],
+            ['name' => 'Elaborar evaluaciones de asignatura'],
+            ['name' => 'Administrar evaluaciones de asignatura'],
+            ['name' => 'Calificar evaluaciones de asignatura'],
+            ['name' => 'Atender consultas de asignatura'],
         ]);        
         
         $asesor = Position::create(['name' => 'Asesor de trabajo de graduacion']);
         $asesor->activities()->createMany([
             ['name' => 'Reunion semanal con estudiantes para planificar el contenido del trabajo de graduacion'],
-            ['name' => 'Atender consultas'],
-            ['name' => 'Revisar avances'],
+            ['name' => 'Atender consultas de trabajo de graduacion'],
+            ['name' => 'Revisar avances de trabajo de graduacion'],
+            ['name' => 'Evaluar trabajo de graduacion'],
+            ['name' => 'Calificar trabajo de graduacion'],
             ['name' => 'Programar defensas'],
             ['name' => 'Asistir como evaluador a otros trabajos de graduacion'],
         ]);
