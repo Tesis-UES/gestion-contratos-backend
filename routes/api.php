ce43646ce726465202712d97851e53fca01c4c0d
Walter Luna <mywalter97@gmail.com>
2022-03-09 21:25:01 -0600

    ✏️ Fix typo on Route

--- routes/api.php
+++ routes/api.php
@@ -382,7 +382,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     });
 
     Route::get('/hiringRequest/all/petitions/hr', [HiringRequestController::class, 'getAllHiringRequestsHR']);
-    Route::post('hiringRequest/{hiringRequest}/sendToHR', [HiringRequestController::class, 'sendToHR']);
+    Route::post('/hiringRequest/{hiringRequest}/sendToHR', [HiringRequestController::class, 'sendToHR']);
 
     Route::get('/hiringRequestSPNP/{id}/create/PDF/{show}', [HiringRequestController::class, 'MakeHiringRequestSPNP']);
     Route::get('/hiringRequestTI/{id}/create/PDF/{show}', [HiringRequestController::class, 'MakeHiringRequestTiempoIntegral']);
