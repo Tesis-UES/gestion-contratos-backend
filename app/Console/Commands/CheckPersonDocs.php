8f21b58fc54339a0fe55474d58ad59a0206ae450
AlexanderCornejo <guillermobrs@gmail.com>
2022-03-25 18:04:39 -0600

    validator

--- app/Console/Commands/CheckPersonDocs.php
+++ app/Console/Commands/CheckPersonDocs.php
@@ -5,28 +5,15 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpKernel\HttpCache\Store;
+use App\Models\Person;
+use App\Mail\ValidationDocsNotification;
 
 class CheckPersonDocs extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'command:checkPersonDocs';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Comando que revisa si los documentos de las personas estan vigentes segun la fecha de vencimiento';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
@@ -39,7 +26,11 @@ class CheckPersonDocs extends Command
      */
     public function handle()
     {
-        $texto = "PRUEBA PRELIMINAR 1";
-        Storage::append('logs.txt', $texto);	
+       $candidates = Person::all();
+         foreach ($candidates as $candidate) {
+            if ($candidate->doc_expiration_date < date('Y-m-d')) {
+            }
+            \Log::info($candidates);
+         }
     }
 }
